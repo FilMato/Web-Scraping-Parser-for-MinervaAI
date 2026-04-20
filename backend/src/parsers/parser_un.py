bf925136bf9ef27bd7c5fe67d0ca7e30e638ff3a
cdb245 <chiaradebenedictis24@gmail.com>
2026-04-20 14:32:59 +0200

    domini_gs.json

--- backend/src/parsers/parser_un.py
+++ backend/src/parsers/parser_un.py
@@ -18,62 +18,111 @@ class Parser_UN(Parser):
                 ".radix-layouts-content", 
                 ".post-content",          
                 ".field-name-body",  
-                ".main-container container"     
+                ".main-container container",     
                 "#main-content",
                 "#content",              
-                "#main",
-                ""                       
+                "#main"                     
             ]
-        
 
     @staticmethod
     def clean_output(text: str):
+
         text = re.sub(r'\[\]\([^)]+\)', '', text) #clear empty markdown links
         return text
     
-    async def parser_url(self, url: str) -> dict: #input url, output json obj
+    @staticmethod
+    def _get_title(text: str) -> str:
 
-        path = urlparse(url).path
-        urlname = os.path.basename(path)
-        titolo = os.path.splitext(urlname)[0].replace("-", " ").capitalize()
+        match_str = re.search(r'\<title>(.+)<\/title>', text)
+        return match_str.group(1) if match_str else "no matches!"
 
-        browser_cfg = BrowserConfig(headless=False)
-       
+    
+    async def parser_url(self, url: str, raw_html: str) -> dict: #input url, output json obj
+
+        domain = urlparse(url).netloc
+        browser_cfg = BrowserConfig(headless = True)
+        title = self._get_title(raw_html)
         md_generator = DefaultMarkdownGenerator(
             options={
                 "ignore_images": True
             },
             content_source= "cleaned_html"
         )
+        no_selector_cfg = CrawlerRunConfig(cache_mode=CacheMode.BYPASS,
+                                            exclude_all_images= True,
+                                            exclude_social_media_links=True,
+                                            excluded_tags=['title', 'nav', 'header', 'footer','button', 'video'],
+                                            excluded_selector=".views-field-field-news-tags, .block-content-footer, .type-entermedia_image, #player-gui, #addtoany, #sharing_widget, #skip-link, .image-caption, #sharing-widget, #breadcrumbs, #more_button, .photo-credit, .page-header, .fusion-video, #player-controls, .wp-caption-text",
+                                            markdown_generator=md_generator)
         
         async with AsyncWebCrawler(config=browser_cfg) as crawler:
-        
+
             for selector in self.css_selectors:
-                crawler_cfg_ok = CrawlerRunConfig(cache_mode=CacheMode.BYPASS,
-                                                css_selector=selector,
-                                                exclude_all_images= True,
-                                                exclude_social_media_links=True,
-                                                excluded_tags=['img', 'nav', 'header', 'footer','button', 'video'],
-                                                excluded_selector=".views-field-field-news-tags, .block-content-footer, .type-entermedia_image, #player-gui, #addtoany, #sharing_widget, #skip-link, .image-caption, #sharing-widget, #breadcrumbs, #more_button, .photo-credit, .page-header, .fusion-video, #player-controls, .wp-caption-text",
-                                                markdown_generator=md_generator
-                                            )
+                selector_cfg = CrawlerRunConfig(cache_mode=CacheMode.BYPASS,
+                                        css_selector=selector,
+                                        exclude_all_images= True,
+                                        exclude_social_media_links=True,
+                                        excluded_tags=['nav', 'header', 'footer','button', 'video'],
+                                        excluded_selector=".views-field-field-news-tags, .block-content-footer, .type-entermedia_image, #player-gui, #addtoany, #sharing_widget, #skip-link, .image-caption, #sharing-widget, #breadcrumbs, #more_button, .photo-credit, .page-header, .fusion-video, #player-controls, .wp-caption-text",
+                                        markdown_generator=md_generator)
                 result = await crawler.arun(
-                    url = url, config=crawler_cfg_ok
+                    url = f'raw:{raw_html}', 
+                    config = selector_cfg
                 )
                 result_markdown = self.clean_output(result.markdown)
                 if result.success and result_markdown and len(result_markdown.strip()) > 50:
+                    print(title)
                     return {
                         "url": url,
-                        "domain": "it.wikipedia.org",
-                        "title": titolo,
+                        "domain": domain,
+                        "title": title,
                         "parsed_text": result_markdown,
                         "html_text": result.html or ""   
                     }
                 
+            result = await crawler.arun( #se non dovesse esistere alcun selector faccio il parsing un'ultima volta senza alcun selector
+                url = f'raw:{raw_html}', 
+                config = no_selector_cfg 
+            )
+            result_markdown = self.clean_output(result.markdown)
+            if result.success and result_markdown and len(result_markdown.strip()) > 50:
+                    print(title)
+                    return {
+                        "url": url,
+                        "domain": domain,
+                        "title": title,
+                        "parsed_text": result_markdown,
+                        "html_text": result.html or ""   
+                    }
             return {
                 "url": url,
-                "domain": "it.wikipedia.org",
+                "domain": domain,
                 "title": "Errore di parsing",
                 "parsed_text": "",
                 "html_text": ""
             }  
+        
+async def main():
+    url = "https://www.un.org/en/observances/asteroid-day"
+    browser_config = BrowserConfig(headless = False)
+    run_config = CrawlerRunConfig(cache_mode=CacheMode.BYPASS)
+
+    async with AsyncWebCrawler(config= browser_config) as crawler:
+        result = await crawler.arun(url = url, config=run_config)
+    
+    ps = Parser_UN()
+    
+    # Await the coroutine and capture the returned dictionary
+    parsed_data = await ps.parser_url(url, result.html)
+    
+    # Check if text was parsed, then write to txt file
+    if parsed_data.get("parsed_text"):
+        with open("output.txt", "w", encoding="utf-8") as f:
+            f.write(parsed_data["parsed_text"])
+        print("Success! Parsed text saved to output.txt")
+    else:
+        print("Warning: No parsed text to save.")
+        
+if __name__ == "__main__":
+    # Standard entry point. Keep all the async logic inside main()
+    asyncio.run(main())
---
+++ gs_writer.py
@@ -0,0 +1,190 @@
+import json
+import asyncio
+from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig, CacheMode
+
+#with this function we create a dictionary that contains all the json data
+async def json_creator(url: str, domain: str, title: str, gold_text: str): #crawler is only used to retrieve the html text
+
+    json_dict = {}
+    browser_cfg = BrowserConfig(headless=False) #if true the page opened does not show
+    crawler_cfg = CrawlerRunConfig(cache_mode=CacheMode.BYPASS)
+
+    async with AsyncWebCrawler(config=browser_cfg) as crawler:
+
+        result = await crawler.arun(
+            url=url, config=crawler_cfg
+            )
+
+        json_dict["url"] = url
+        json_dict["domain"] = domain
+        json_dict["title"] = title
+        json_dict["html_text"] = result.html
+        json_dict["gold_text"] = gold_text
+
+        return json_dict
+
+urls_to_process = [
+    {"url" : "https://it.wikipedia.org/wiki/Roma", 
+     "domain" : "it.wikipedia.org", 
+     "title" : "Roma", 
+     "golden_text" : "gs_data/wikipedia_gs_txt/Roma_gs.txt"},
+
+     {"url" : "https://it.wikipedia.org/wiki/Sistema_solare", 
+     "domain" : "it.wikipedia.org", 
+     "title" : "Sistema Solare", 
+     "golden_text" : "gs_data/wikipedia_gs_txt/Sistema_Solare_gs.txt"},
+
+     {"url" : "https://it.wikipedia.org/wiki/Permesso_di_soggiorno_UE_per_soggiornanti_di_lungo_periodo", 
+     "domain" : "it.wikipedia.org", 
+     "title" : "Permesso di soggiorno UE per soggiornanti di lungo periodo", 
+     "golden_text" : "gs_data/wikipedia_gs_txt/GS_PermessoSoggiorno.txt"},
+
+    {"url" : "https://it.wikipedia.org/wiki/Mercurio_(elemento_chimico)", 
+     "domain" : "it.wikipedia.org", 
+     "title" : "Mercurio (elemento chimico)", 
+     "golden_text" : "gs_data/wikipedia_gs_txt/Mercurio(elemento_chimico)_gs.txt"},
+
+     {"url" : "https://it.wikipedia.org/wiki/Seconda_guerra_mondiale", 
+     "domain" : "it.wikipedia.org", 
+     "title" : "Seconda guerra mondiale", 
+     "golden_text" : "gs_data/wikipedia_gs_txt/Seconda_Guerra_Mondiale_gs.txt"},
+
+     {"url" : "https://it.wikipedia.org/wiki/Leonardo_da_Vinci", 
+     "domain" : "it.wikipedia.org", 
+     "title" : "Leonardo da Vinci", 
+     "golden_text" : "gs_data/wikipedia_gs_txt/Leonardo_da_Vinci_gs.txt"},
+
+     {"url" : "https://it.wikipedia.org/wiki/Equazione_di_Schr%C3%B6dinger", 
+     "domain" : "it.wikipedia.org", 
+     "title" : "Equazione di Schrödinger", 
+     "golden_text" : "gs_data/wikipedia_gs_txt/Equazione_di_Schrodinger_gs.txt"},
+
+     {"url" : "https://it.wikipedia.org/wiki/Massa_(fisica)", 
+     "domain" : "it.wikipedia.org", 
+     "title" : "Massa (fisica)", 
+     "golden_text" : "gs_data/wikipedia_gs_txt/Massa_(fisica)_gs.txt"},
+
+     {"url" : " https://it.wikipedia.org/wiki/Antico_Egitto", 
+     "domain" : "it.wikipedia.org", 
+     "title" : "Antico Egitto", 
+     "golden_text" : "gs_data/wikipedia_gs_txt/Antico_Egitto_gs.txt"},
+
+     {"url" : "https://www.un.org/fr/about-us/history-of-the-un/san-francisco-conference", 
+     "domain" : "un.org", 
+     "title" : "La Conférence de San Francisco", 
+     "golden_text" : "gs_data/un_gs_txt/La_Conference_de_San_Francisco_gs.txt"},
+
+     {"url" : "https://www.un.org/es/common-agenda/implementation", 
+     "domain" : "un.org", 
+     "title" : "Implementacion", 
+     "golden_text" : "gs_data/un_gs_txt/Implementacion_gs.txt"},
+
+     {"url" : "https://www.un.org/unispal/history/", 
+     "domain" : "un.org", 
+     "title" : "History of the Question of Palestine", 
+     "golden_text" : "gs_data/un_gs_txt/History_of_the_question_of_palestine_gs.txt"},
+
+     {"url" : "https://www.un.org/en/observances/asteroid-day", 
+     "domain" : "un.org", 
+     "title" : "International Asteroid Day 30 June", 
+     "golden_text" : "gs_data/un_gs_txt/International_Asteroid_day_30_June_gs.txt"},
+
+     {"url" : "https://www.un.org/peacebuilding/gender-strategy", 
+     "domain" : "un.org", 
+     "title" : "The Peacebuilding Commission's Gender Strategy", 
+     "golden_text" : "gs_data/un_gs_txt/The_Peacebuilding_Commissions_Gender_Strategy_gs.txt"},
+
+     {"url" : "https://www.un.org/en/about-us/universal-declaration-of-human-rights", 
+     "domain" : "un.org", 
+     "title" : "Universal Declaration of Human Rights", 
+     "golden_text" : "gs_data/un_gs_txt/Universal_Declaration_of_Human_Rights_gs_txt"},
+
+     {"url" : "https://www.un.org/en/climatechange/science/mythbusters", 
+     "domain" : "un.org", 
+     "title" : "Myth Busters", 
+     "golden_text" : "gs_data/un_gs_txt/Myth_Busters_gs.txt"},
+
+     {"url" : "https://www.my-personaltrainer.it/salute-benessere/cervello.html", 
+     "domain" : "www.my-personaltrainer.it", 
+     "title" : "Cervello", 
+     "golden_text" : "gs_data/mypersonaltrainer_gs/cervello.txt"
+    },
+
+    {"url" : "https://www.my-personaltrainer.it/ETICHETTE-NUTRIZIONALI.htm", 
+    "domain" : "www.my-personaltrainer.it", 
+    "title" : "Etichette nutrizionali", 
+    "golden_text" : "gs_data/mypersonaltrainer_gs/etichette_nutrizionali.txt"
+    },
+
+    {"url" : "https://www.my-personaltrainer.it/nutrizione/malnutrizione.html", 
+    "domain" : "www.my-personaltrainer.it", 
+    "title" : "Malnutrizione", 
+    "golden_text" : "gs_data/mypersonaltrainer_gs/malnutrizione.txt"
+    },
+
+    {"url" : "https://www.my-personaltrainer.it/Tv/Ricette/Dolci_Dessert/torta-alla-ricotta-senza-farina.html", 
+    "domain" : "www.my-personaltrainer.it", 
+    "title" : "Torta alla ricotta senza farina", 
+    "golden_text" : "gs_data/mypersonaltrainer_gs/torta_ricotta.txt"
+    },
+
+    {"url" : "https://www.my-personaltrainer.it/nutrizione/vitamine-minerali.html", 
+    "domain" : "www.my-personaltrainer.it", 
+    "title" : "Vitamine minerali", 
+    "golden_text" : "gs_data/mypersonaltrainer_gs/vitamine.txt"
+    },
+
+    {"url" : "https://www.premierleague.com/en/tables/premier-league/2024-25/all-matchweeks", 
+     "domain" : "premierleague.com", 
+     "title" : "Table", 
+     "golden_text" : "gs_data/premierleague_gs/classifica_gs.txt" 
+    },
+
+    {"url" : "https://www.premierleague.com/en/transfers/2025-26/january", 
+        "domain" : "premierleague.com", 
+        "title" : "Transfer Watch", 
+        "golden_text" : "gs_data/premierleague_gs/transfer_gs.txt" 
+    },
+
+    {"url" : "https://www.premierleague.com/en/news/4324979", 
+        "domain" : "premierleague.com", 
+        "title" : "Strong, fearless - Is midfield role next for Lewis-Skelly after new deal?", 
+        "golden_text" : "gs_data/premierleague_gs/role_Lewis-Kelly_gs.txt"
+    },
+
+    {"url" : "https://www.premierleague.com/en/news/3533373", 
+        "domain" : "premierleague.com", 
+        "title" : "Season trends: Record low number of red cards", 
+        "golden_text" : "gs_data/premierleague_gs/red_cards_gs.txt"
+    }
+]
+
+for current_page in urls_to_process:
+
+    curr_url = current_page["url"]
+    curr_domain = current_page["domain"]
+    curr_title = current_page["title"]
+    with open(current_page["golden_text"], "r", encoding="utf-8") as f: #gs.txt is a general txt file that contains the text manually pasted from the selected url
+        curr_gold_text = f.read()
+
+    json_data = asyncio.run(json_creator(curr_url,curr_domain,curr_title,curr_gold_text))
+
+    #json returns a python list
+    try:
+        with open(f"dominio_{curr_domain}_gs.json", "r", encoding="utf-8") as r_file:
+            data = json.load(r_file)
+    except FileNotFoundError:
+        data = []
+
+    data.append(json_data)
+
+    #write whole list to json file
+    with open(f"dominio_{curr_domain}_gs.json", "w", encoding="utf-8") as a_file:
+        json.dump(data, a_file, indent=2)
+
+
+
+
+
+
+
